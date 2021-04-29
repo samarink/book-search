f43c30f7c1bdbf25107fe3d3f969ef18fc3936f4
Konstantin Samarin <konstsamari@gmail.com>
2021-04-29 19:04:16 +0700

    add basic styling to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import theme from '../theme';
 import { useDispatch } from 'react-redux';
 import { useDebounce } from 'use-debounce';
 import { fetchBooksBySearchTerm } from '../actions/booksActions';
+
+const Input = styled.input`
+  outline: none;
+  width: 100%;
+  box-sizing: border-box;
+  padding: ${theme.paddings.small} ${theme.paddings.medium};
+  margin-top: ${theme.margins.normal};
+  margin-bottom: ${theme.margins.big};
+  border: 2px solid ${theme.colors.lightgray};
+  border-radius: 8px;
+  box-shadow: 5px 5px 18px #888888;
+  font-size: ${theme.fontSizes.normal};
+  font-weight: ${theme.fontWeights.bold}
+
+  transition: border 0.2s ease;
+  &:focus,
+  &:hover {
+    border: 2px solid ${theme.colors.lightblue};
+  }
+`;
 
 const SearchBar = () => {
   const dispatch = useDispatch();
@@ -15,15 +37,19 @@ const SearchBar = () => {
 
   const handleSearch = () => {
     dispatch(fetchBooksBySearchTerm(debouncedSearchTerm));
-  }
+  };
 
   useEffect(() => {
     handleSearch();
-  }, [debouncedSearchTerm])
+  }, [debouncedSearchTerm]);
 
   return (
     <>
-      <input value={searchTerm} onChange={onChange} />
+      <Input
+        value={searchTerm}
+        onChange={onChange}
+        placeholder="e.g The Lord Of The Rings.."
+      />
     </>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,8 @@ const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#586069',
+    lightgray: '#dbdbdb',
+    lightblue: '#64b4f5'
   },
   fontSizes: {
     normal: '16px',
@@ -11,9 +13,15 @@ const theme = {
     normal: '400',
     bold: '700',
   },
+  paddings: {
+    small: '12px',
+    normal: '16px',
+    medium: '24px',
+    big: '32px',
+  },
   margins: {
     normal: '16px',
-    big: '32px'
+    big: '32px',
   },
 };
 
