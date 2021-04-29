d59161ca4b3605ff217c927a96911a089ad46cf0
Konstantin Samarin <konstsamari@gmail.com>
2021-04-29 21:37:31 +0700

    remove unnecessary pading on BooksList

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import BookEntry from './BookEntry';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+
+const Container = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
 
 const BookList = () => {
   const books = useSelector((state) => state.books.all);
 
   return (
-    <ul>
+    <Container>
       {books.map((book) => (
         <BookEntry book={book} key={book.key} />
       ))}
-    </ul>
+    </Container>
   );
 };
 
