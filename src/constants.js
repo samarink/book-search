f772622d82f7b4908cbe66d556272a8de7c0cbf1
Konstantin Samarin <konstsamari@gmail.com>
2021-04-29 17:01:40 +0700

    change book search api url constant
    perform the search by title instead of a generic search

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BOOK_SEARCH_API_URL = 'https://openlibrary.org/search.json?q=';
+export const BOOK_SEARCH_API_URL = 'https://openlibrary.org/search.json?title=';
 export const BOOK_COVER_API_URL = 'http://covers.openlibrary.org/b/id/';
