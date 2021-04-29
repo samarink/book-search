5e368fc14a4d94d1befab10e6d3e9b996e879717
Konstantin Samarin <konstsamari@gmail.com>
2021-04-29 19:31:52 +0700

    write custom useDebounce (#1)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useDebounce } from 'use-debounce';
+import useDebounce from '../hooks/useDebounce';
 import { fetchBooksBySearchTerm } from '../actions/booksActions';
 import Input from './Input';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState('');
-  const [debouncedSearchTerm] = useDebounce(searchTerm, 1000);
+  const debouncedSearchTerm = useDebounce(searchTerm, 1000);
 
   const onChange = (event) => {
     event.preventDefault();
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useDebounce = (value, timeout) => {
+  let [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, timeout);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, timeout]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
