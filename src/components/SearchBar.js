bfd387302d7b8539ab0d90a13477adf48eefce2e
Konstantin Samarin <konstsamari@gmail.com>
2021-04-29 15:43:13 +0700

    debounce searchTerm
    don't perform the search unless user hasn't typed for at least a second

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useDebounce } from 'use-debounce';
 import { fetchBooksBySearchTerm } from '../actions/booksActions';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 1000);
 
   const onChange = (event) => {
     event.preventDefault();
     setSearchTerm(event.target.value);
-    dispatch(fetchBooksBySearchTerm(searchTerm));
   };
+
+  const handleSearch = () => {
+    dispatch(fetchBooksBySearchTerm(debouncedSearchTerm));
+  }
+
+  useEffect(() => {
+    handleSearch();
+  }, [debouncedSearchTerm])
 
   return (
     <>
