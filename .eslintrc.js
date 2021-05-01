d2d1643042cb50eac52157da53e79f891ca76a58
Konstantin Samarin <konstsamari@gmail.com>
2021-05-01 19:14:55 +0700

    set up linting
    better late than never

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/prop-types': 'off',
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+  },
+};
--- src/utils.js
+++ src/utils.js
@@ -9,8 +9,8 @@ export const getBookCoverUrlWithCoverI = (cover_i, size = 'medium') => {
 
   return cover_i && cover_i !== -1
     ? `${BOOK_COVER_API_URL}${cover_i}${sizes[size]}.jpg`
-    : // use placeholder cover in case no actual cover was provided
-      'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/book.png';
+    : 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/book.png';
+  // use placeholder cover in case no actual cover was provided
 };
 
 export const formatAuthorName = (authors) => formatter(authors);
